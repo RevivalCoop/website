2a2bd9b52228415dcb28c360a5e58ec4de57dc1d
Sean Beck <spcbeck@gmail.com>
2019-03-12 23:10:01 -0400

    Adding work section.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700" rel="stylesheet">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
